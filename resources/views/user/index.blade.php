4d76e80508e9f3cb0559606d399fa14f525541d3
hannasabrynna <hannasabrynna135@gmail.com>
2023-07-07 23:14:05 -0300

    deixando a lista de usuarios do administrador mais bonitinha

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,29 +2,33 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
 
-                Olá, {{ Auth::user()->name }}! <br>
+            Olá, {{ Auth::user()->name }}! <br>
 
-                        </h2>
-                        <p> Você está logado com {{ Auth::user()->email }}. <br>
-                {{ \Carbon\Carbon::now()->format('d/m/Y') }}
-                        </p>
+        </h2>
+        <p> Você está logado com {{ Auth::user()->email }}. <br>
+            {{ \Carbon\Carbon::now()->format('d/m/Y') }}
+        </p>
 
 
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-amber-50 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-
-                    <h1 class="text-2xl mt-4 font-bold text-center"> Usuários</h1>
-
+                    <div class="mt-4 text-left ml-5">
+                        <img src="{{ asset('logo.png') }}" alt="imagem do logotipo" style="width: 100px; margin-right: 0px;">
+                        <h1 class="text-lg font-bold ">Feira Na Mão</h1>
+                        <h1 class="text-3xl mt-4 font-bold text-center text-green-800 underline"> Usuários</h1>
+                    </div>
+                   
+                    <br>
                     @if(Auth::user() ->type=='administrador')
                     @foreach (App\Models\user::all() as $user)
-                    <div class="flex justify-between border-b mb-2 gap-4
-                    hover:bg-gray-300" x-data=" { showDelete: false, showEdit: false  } ">
+                    <div class="flex justify-between border-b border-green-800 mb-2 gap-4
+                    hover:bg-lime-100" x-data=" { showDelete: false, showEdit: false  } ">
 
-                        <div class="flex justify-between flex-grow">
+                        <div class="flex justify-between flex-grow text-lg">
 
 
                             Nome: {{ $user-> name }} <br>
@@ -34,33 +38,33 @@
 
 
                             @if(Auth::user() ->email == $user-> email)
-                            <p>Conta que você está logado atualmente</p>
+                            <p class="mt-5 mr-5 text-green-800 underline">Conta que você está logado atualmente</p>
                             @endif
                         </div>
 
-                        <div class="flex gap-2">
+                        <div class="flex gap-2 mt-5">
                             <div>
-                                <span class="cursor-pointer border rounded-md  px-2 bg-red-500 text-white" @click="showDelete = true ">Apagar</span>
+                                <span class="cursor-pointer border rounded-md px-2 bg-red-500 text-white" @click="showDelete = true ">Apagar</span>
                             </div>
                         </div>
 
 
-                <template x-if="showDelete">
-                <div class="fixed inset-0 flex items-center justify-center z-50">
-                    <div class="absolute inset-0 bg-gray-800 bg-opacity-20"></div>
-                    <div class="w-96 bg-white p-4 relative z-10">
-                        <h2 class="text-xl font-bold text-center">Você tem certeza que quer apagar?</h2>
-                        <form action="{{ route('user.destroy', $user) }}" method="POST">
-                            @csrf
-                            @method('delete')
-                            <x-danger-button class="bg-red-300 hover:bg-red-500">Apagar</x-danger-button>
-                        </form>
-                        <x-primary-button class="w-full" @click="showDelete = false">Cancelar</x-primary-button>
+                        <template x-if="showDelete">
+                            <div class="fixed inset-0 flex items-center justify-center z-50">
+                                <div class="absolute inset-0 bg-gray-800 bg-opacity-20"></div>
+                                <div class="w-96 bg-white p-4 relative z-10">
+                                    <h2 class="text-xl font-bold text-center">Você tem certeza que quer apagar?</h2>
+                                    <form action="{{ route('user.destroy', $user) }}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <x-danger-button class="bg-red-300 hover:bg-red-500">Apagar</x-danger-button>
+                                    </form>
+                                    <x-primary-button class="w-full" @click="showDelete = false">Cancelar</x-primary-button>
+                                </div>
+                            </div>
                     </div>
+                    </template>
                 </div>
-                </div>
-            </template>
-        </div>
 
 
                 @endforeach
