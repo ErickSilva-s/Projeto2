a2a70977245f2ef35353c3889f78d7eb6f1343e5
eri123silva2 <erickdasilvasouza8242@gmail.com>
2023-07-08 18:17:43 -0300

    up

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,8 +26,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
 
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verificação de Email Feira na Mão')
+                ->line("Olá,  obrigado por se cadastrar no Feira na Mão, mas pra continuar precisamos verificar seu email. Para isso, clique no botão abaixo. Caso ao apertar no botão  e já esteja logado, saia da sua conta, clique no botão novamente e faça seu login. ")
+                ->action('Verificar Email', $url);
+        });
     }
+
+
 }
